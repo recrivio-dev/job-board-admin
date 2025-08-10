ae193beb62e356cff86c8a4fece91617934f8dc9
redhunt <sarthak1730@gmail.com>
2025-08-11 00:43:16 +0530

    add placeholder for inbox

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -1,10 +1,25 @@
+"use client"
 import Breadcrumb from "@/components/Breadcrumb";
+import { useAppSelector } from "@/store/hooks";
 
 export default function Inbox() {
+  const collapsed = useAppSelector((state) => state.ui.sidebar.collapsed);
   return (
     <div>
-      <Breadcrumb segments={[{ label: "Inbox" }]} />
-      <h1 className="text-9xl text-center">Inbox</h1>
+
+      <div
+        className={`transition-all duration-300 h-full px-3 md:px-6 ${
+          collapsed ? "md:ml-20" : "md:ml-60"
+        } pt-18`}
+      >
+        
+        <div className="w-full mx-auto px-0 md:px-4 py-4 md:py-2">
+          <Breadcrumb segments={[{ label: "Inbox" }]} />
+          <div>
+            Inbox feature is under development. Stay tuned for updates!
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
